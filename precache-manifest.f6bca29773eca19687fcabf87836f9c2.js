45e317e35856bb71280b124813e37357a8ce150b
Sveta K <test@test.test>
2020-01-31 14:09:24 +0000

    Updates

---
+++ precache-manifest.f6bca29773eca19687fcabf87836f9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ca43ee05d127d6f5de0c03dcabcca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d210c40fb18511e2508c",
+    "url": "/static/css/2.36592687.chunk.css"
+  },
+  {
+    "revision": "eba38500e83349b85082",
+    "url": "/static/css/main.765272a3.chunk.css"
+  },
+  {
+    "revision": "d210c40fb18511e2508c",
+    "url": "/static/js/2.32af73ec.chunk.js"
+  },
+  {
+    "revision": "eba38500e83349b85082",
+    "url": "/static/js/main.f081bf61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
