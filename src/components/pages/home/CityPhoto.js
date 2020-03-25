64eaf23b32ea9a006727ed77a049c1dea3045586
Sveta K <test@test.test>
2020-03-25 01:11:02 +0000

    Change styles and photos for CityPhoto.

--- src/components/pages/home/CityPhoto.js
+++ src/components/pages/home/CityPhoto.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import { Col } from 'reactstrap';
-import {
-    Card, CardImg, CardBody,
-    CardTitle, CardText
-} from 'reactstrap';
 import { Link } from "react-router-dom";
-import { HOTELS_INFO } from "../../hotels-info";
 
-function CityPhoto(props) {
-    let hotelsImages = HOTELS_INFO.map((hotel) => {
-        return (
-            <Col xs="12" sm="3" className="text-center" key={hotel.id}>
-                <Link color="secondary" size="sm" to={"/search?city=" + hotel.city.toLowerCase().replace(" ", "-")} >
-                    <Card>
-                        <CardBody>
-                            <CardTitle >{hotel.city}</CardTitle>
-                            <CardImg className="img-fluid img-thumbnail rounded" src={hotel.main_photo} alt={hotel.name} />
-                            <CardText>Hotel "{hotel.name}"</CardText>
-                        </CardBody>
-                    </Card>
-                </Link>
-            </Col>
-        )
-
-    });
+export default function CityPhoto(props) {
     return (
-        <React.Fragment>
-            {hotelsImages}
-        </React.Fragment>);
+
+        <Link color="secondary" className="" size="sm" to={"/search?city=" + props.hotel.city.toLowerCase().replace(" ", "-")} >
+            <img src={props.hotel.photo} className="image-size img-fluid overflow-hidden m-0 p-0" alt={props.hotel.city + " photo"} />
+        </Link>
+
+    )
+
 };
-export default CityPhoto;
