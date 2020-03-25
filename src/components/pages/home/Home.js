6b23adf45ee3e5d221331686d679dbac12399324
Sveta K <test@test.test>
2020-03-25 21:45:58 +0000

    Add rooms info to Home.

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -10,7 +10,7 @@ export default function Home(props) {
 
     const citiesPhotos = HOTELS_INFO.map((hotel, i) => {
         return (
-            <Row className="row-m-p row-height justify-content-center align-items-center blue-color-bg opacity-08" key={i}>
+            <Row className="row-m-p row-height justify-content-center align-items-center gray-color-bg opacity-08" key={i}>
                 <Col xs="12" className="m-0 p-0">
                     <CityPhoto hotel={hotel} />
                 </Col>
@@ -19,26 +19,48 @@ export default function Home(props) {
 
     });
 
-    const logoURL = require("../../pages/home/images/hotels-logo.png");
+    const citiesRooms = HOTELS_INFO.map((hotel, i) => {
+        return (
+            <li>{hotel.city}: {i + 5}</li>
+        );
+
+    });
+
+    const logoURL = require("../../../images/hotels-logo.png");
 
     return (
         <Container className="p-0 my-0 mx-auto  container-home">
             <Row className="m-0 p-0 row-home justify-content-left align-items-start">
                 <Col md="3" className="text-center col-m-p">
-                    <Row className="row-m-p row-height blue-color-bg">
+                    <Row className="row-m-p row-height gold-color-bg justify-content-center align-items-center">
                         <Col xs="12" className="m-0 p-0">
                             <h1>Welcome!</h1>
-                            <h3>Choose the city!</h3>
+                            <h3>choose the city</h3>
                         </Col>
                     </Row>
-                    <Row className="row-m-p row-height gray-color-bg">
-                        <Col xs="12" className="m-0 p-0">
-                            <Link to="search?city=all">Don't want to choose</Link>
+                    <Row className="row-m-p row-height gray-color-bg justify-content-center align-items-center">
+                        <Col xs="12" className="m-0 p-0 h-100">
+                            <Row className="m-0 p-0 h-50 align-items-center">
+                                <Col xs="12" className="m-0 p-0">
+                                    <Link id="go-to-search" className="" to="search?city=all">Check all hotels</Link>
+                                </Col>
+                            </Row>
+                            <Row className="m-0 p-0 bg-white h-50 align-items-center justify-content-center" >
+                                <Col xs="6"><p className="m-0 p-0">available rooms</p></Col>
+                                <Col xs="6">
+                                    <ul className="list-unstyled m-0 p-0 text-left">
+                                        {citiesRooms}
+                                    </ul>
+                                </Col>
+                            </Row>
+
                         </Col>
                     </Row>
-                    <Row className="row-m-p row-height gold-color-bg">
+                    <Row className="row-m-p row-height blue-color-bg justify-content-center align-items-center">
                         <Col xs="12" className="m-0 p-0">
-                            <img className="image-size img-fluid overflow-hidden m-0 p-0" src={logoURL} alt="logo" />
+                            <Link to={"/"} exact="true">
+                                <img className="image-size img-fluid overflow-hidden m-0 p-0" src={logoURL} alt="logo" />
+                            </Link>
                         </Col>
                     </Row>
                 </Col>
