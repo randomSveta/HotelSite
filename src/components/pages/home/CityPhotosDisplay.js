6a12bcac4c9cfeb4ea59ce6f090ae4b6c9a623a9
Sveta K <test@test.test>
2020-05-02 22:32:06 +0100

    Make city photos as background-image.

--- src/components/pages/home/CityPhotosDisplay.js
+++ src/components/pages/home/CityPhotosDisplay.js
@@ -8,7 +8,7 @@ export default function CityPhotoDisplay(props) {
     const citiesPhotos = HOTELS_INFO.map((hotel, i) => {
         return (
 
-            <Col xs="12" className="basic-row-m-p basic-row-height border d-flex justify-content-center align-items-center basic-dark-bg" key={i}>
+            <Col xs="12" className="basic-row-m-p basic-row-height d-flex justify-content-center align-items-center home-city-image" key={i}>
                 <CityPhoto hotel={hotel} />
             </Col>
         );
